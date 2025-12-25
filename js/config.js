e645caec00d484ffa38a0e608ae993f7fae16dc5
nsantos <nsantos.ks@gmail.com>
2025-12-25 12:04:23 -0400

    Reparar enlace QR de certificados

--- js/config.js
+++ js/config.js
@@ -1,7 +1,9 @@
 // Configuración Centralizada de Gammaliel Academy
 const GA_CONFIG = {
-    // Aquí pegas tu URL del Script UNA SOLA VEZ
+    //GA_CONFIG.APPS_SCRIPT_URL : Url de Appscrip para gestionar la busqueda de certificado
     APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwekynHRksTWWGVMepKVq09zs8c5jdMc1tqIeooouCGgCIzM8tT3IcPGWyqFDGIAxQ/exec",
+    //GA_CONFIG.WHATSAPP_APPSHEET_DEV : Interes en el desarrollo de AppSheet
+    WHATSAPP_APPSHEET_DEV: "https://wa.me/+584128672845?text=Hola,%20estoy%20interesado%20en%20el%20desarrollo%20de%20una%20AppSheet%20para%20mi%20negocio.",
     
     // Puedes agregar más cosas a futuro aquí (ej. versión, autor)
     APP_VERSION: "v3.0"

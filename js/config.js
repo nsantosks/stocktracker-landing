09ed7844ed3a623dc50e4f3219c15290cb7162a0
nsantos <nsantos.ks@gmail.com>
2025-12-25 01:58:36 -0400

    integración de cursos con certificados

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// Configuración Centralizada de Gammaliel Academy
+const GA_CONFIG = {
+    // Aquí pegas tu URL del Script UNA SOLA VEZ
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwekynHRksTWWGVMepKVq09zs8c5jdMc1tqIeooouCGgCIzM8tT3IcPGWyqFDGIAxQ/exec",
+    
+    // Puedes agregar más cosas a futuro aquí (ej. versión, autor)
+    APP_VERSION: "v3.0"
+};

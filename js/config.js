3e9a3a5422774b4362fd93b751d6b1bcc6bb93ac
nsantos <nsantos.ks@gmail.com>
2025-12-30 02:27:39 -0400

    Inclusión de herramienta canvas

--- js/config.js
+++ js/config.js
@@ -4,7 +4,9 @@ const GA_CONFIG = {
     APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwekynHRksTWWGVMepKVq09zs8c5jdMc1tqIeooouCGgCIzM8tT3IcPGWyqFDGIAxQ/exec",
     //GA_CONFIG.WHATSAPP_APPSHEET_DEV : Interes en el desarrollo de AppSheet
     WHATSAPP_APPSHEET_DEV: "https://wa.me/+584128672845?text=Hola,%20estoy%20interesado%20en%20el%20desarrollo%20de%20una%20AppSheet%20para%20mi%20negocio.",
-    
+    //GA_CONFIG.APPS_SCRIPT_URL_CANVAS : Url de Appscrip para gestionar el registro de CANVAS DE NEGOCIO
+    APPS_SCRIPT_URL_CANVAS: "https://script.google.com/macros/s/AKfycbzEQfUWXSQJQtsfKKWmd33vN9o45Ccfoaydke4UG38n4bjBtdBuqDu11a5NABlwXOZB/exec",
+
     // Puedes agregar más cosas a futuro aquí (ej. versión, autor)
     APP_VERSION: "v3.0"
 };

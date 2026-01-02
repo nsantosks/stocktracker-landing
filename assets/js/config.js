a3780f19725e28bd6f502bfae99ee77bb9a8a522
nsantos <nsantos.ks@gmail.com>
2026-01-01 20:34:09 -0400

    Actualización del SEO del index y adicion de paginas de consultores

---
+++ assets/js/config.js
@@ -0,0 +1,21 @@
+// Configuración Centralizada de Gammaliel Academy
+const GA_CONFIG = {
+    //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    //::::::::::::::::::LINKS DE APPSCRIPT Y WEBSAPPS EN GOOGLE CLOUD::::::::::::::::::::
+    //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    //GA_CONFIG.APPS_SCRIPT_URL : Url de Appscrip para gestionar la busqueda de certificado
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwekynHRksTWWGVMepKVq09zs8c5jdMc1tqIeooouCGgCIzM8tT3IcPGWyqFDGIAxQ/exec",
+    //GA_CONFIG.APPS_SCRIPT_URL_CANVAS : Url de Appscrip para gestionar el registro de CANVAS DE NEGOCIO
+    APPS_SCRIPT_URL_CANVAS: "https://script.google.com/macros/s/AKfycbzEQfUWXSQJQtsfKKWmd33vN9o45Ccfoaydke4UG38n4bjBtdBuqDu11a5NABlwXOZB/exec",
+
+    //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    //::::::::::::::::::LINKS DE WHASAPP EN BOTONES FLOTANTES::::::::::::::::::::::::::::
+    //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    //GA_CONFIG.WHATSAPP_APPSHEET_DEV : Interes en el desarrollo de AppSheet
+    WHATSAPP_APPSHEET_DEV: "https://wa.me/+584128672845?text=Hola,%20estoy%20interesado%20en%20el%20desarrollo%20de%20una%20AppSheet%20para%20mi%20negocio.",
+    
+    // Puedes agregar más cosas a futuro aquí (ej. versión, autor)
+    APP_VERSION: "v4.0"
+};
